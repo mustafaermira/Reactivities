b216f0c342d1ffcc8aff34a759a8e21fbf9e9282
ermira mustafa <mustafaermira@gmail.com>
2021-05-27 14:19:40 +0200

    End of section 3

--- API/Startup.cs
+++ API/Startup.cs
@@ -35,6 +35,11 @@ namespace API
             services.AddDbContext<DataContext>(opt=> {
                 opt.UseSqlite(Configuration.GetConnectionString("DefaultConnection"));
             });
+            services.AddCors(opt => {
+                opt.AddPolicy("CorsPolicy", policy => {
+                    policy.AllowAnyHeader().AllowAnyMethod().WithOrigins("http://localhost:3000");
+                });
+            });
             services.AddMvc().SetCompatibilityVersion(CompatibilityVersion.Version_2_2);
         }
 
@@ -52,6 +57,7 @@ namespace API
             }
 
             //app.UseHttpsRedirection();
+            app.UseCors("CorsPolicy");
             app.UseMvc();
         }
     }
