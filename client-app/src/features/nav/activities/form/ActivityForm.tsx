4f697c4af4fe6104eb846e7bf4a2e2f9eee3f449
ermira mustafa <mustafaermira@gmail.com>
2021-05-29 22:27:13 +0200

    End of section 6

--- client-app/src/features/nav/activities/form/ActivityForm.tsx
+++ client-app/src/features/nav/activities/form/ActivityForm.tsx
@@ -8,9 +8,10 @@ interface IProps {
     activity: IActivity;
     createActivity: (activity: IActivity) => void;
     editActivity: (activity:IActivity) => void;
+    submitting:boolean;
 }
 
-const ActivityForm:React.FC<IProps> = ({setEditMode, activity:initializeFormState, createActivity, editActivity}) => {
+const ActivityForm:React.FC<IProps> = ({setEditMode, activity:initializeFormState, createActivity, editActivity, submitting}) => {
 
     const initializeForm = () => {
         if (initializeFormState) {
@@ -58,7 +59,7 @@ const ActivityForm:React.FC<IProps> = ({setEditMode, activity:initializeFormStat
                 <Form.Input onChange={handleInputChange} name='date' type='datetime-local' placeholder='Date' value={activity.date}/>
                 <Form.Input onChange={handleInputChange} name='city' placeholder='City' value={activity.city}/>
                 <Form.Input onChange={handleInputChange} name='venue' placeholder='Venue' value={activity.venue}/>
-                <Button floated='right' positive type='submit' content='Submit'/>
+                <Button loading={submitting} floated='right' positive type='submit' content='Submit'/>
                 <Button onClick={() => setEditMode(false)} floated='right' type='button' content='Cancel'/>
             </Form>
         </Segment>
